7a28a8d9625c5113df41d24171b2d30cd04d7eb5
i-m-afk <rishavkumar208@gmail.com>
2023-03-15 21:17:52 +0530

    Binary Tree representation

---
+++ Tree/BinaryTree.java
@@ -0,0 +1,20 @@
+class Node {
+    int val;
+    Node left;
+    Node right;
+
+    Node(int val) {
+        this.val = val;
+    }
+}
+
+public class BinaryTree {
+    public static void main(String[] args) {
+        Node root = new Node(30);
+        root.left = new Node(40);
+        root.right = new Node(50);
+        root.left.left = new Node(70);
+        root.right.left = new Node(60);
+        root.right.right = new Node(80);
+    }
+}
