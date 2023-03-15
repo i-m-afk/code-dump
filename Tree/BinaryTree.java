f0acb179c54b004ba5c6c37e6fab521987095d3a
i-m-afk <rishavkumar208@gmail.com>
2023-03-15 21:51:19 +0530

    Update BinaryTree.java
    removed Node class and declared it separately

--- Tree/BinaryTree.java
+++ Tree/BinaryTree.java
@@ -1,13 +1,3 @@
-class Node {
-    int val;
-    Node left;
-    Node right;
-
-    Node(int val) {
-        this.val = val;
-    }
-}
-
 public class BinaryTree {
     public static void main(String[] args) {
         Node root = new Node(30);
